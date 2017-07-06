634f35fe5c0b8935ee4dc26e53a0c3e5fd66ba90
John Stewien <stewienj@users.noreply.github.com>
2017-07-06 16:54:38 +0930

    Removed dependencies on PresentationFramework and WindowsBase

--- Swordfish.NET.CollectionsV3/ConcurrentObservableBase.cs
+++ Swordfish.NET.CollectionsV3/ConcurrentObservableBase.cs
@@ -3,11 +3,8 @@ using System;
 using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-using System.Windows.Threading;
 
 namespace Swordfish.NET.Collections
 {
