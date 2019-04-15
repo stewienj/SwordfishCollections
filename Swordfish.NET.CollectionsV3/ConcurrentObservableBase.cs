5a8597703b80b37fa129374a3579de40b1cb1a4e
John Stewien <stewienj@users.noreply.github.com>
2019-04-15 15:15:00 +0930

    Added some handy debug info to the potential data binding exception in ConcurrentObservableBase

--- Swordfish.NET.CollectionsV3/ConcurrentObservableBase.cs
+++ Swordfish.NET.CollectionsV3/ConcurrentObservableBase.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Runtime.Serialization;
 using System.Text;
@@ -42,8 +43,37 @@ namespace Swordfish.NET.Collections
     /// </summary>
     private ThrottledAction _viewChanged;
 
+    /// <summary>
+    /// When built as DEBUG, the constructor stores stack frames here which can be used to find an object
+    /// </summary>
+    private StackFrame[] _stackFrames;
+
     protected ConcurrentObservableBase(bool isMultithreaded, TInternalCollection initialCollection)
     {
+      // If compiled as debug, then store the stack trace as an aid to working out which object this is in case of a binding error.
+      // This is non performant, so only use this in debug builds.
+#if DEBUG
+      // This function checks if the declaring class of a method in a stack frame inherits from the base type name passed in.
+      // Was unable to use Type.IsSubClassOf() due to the type being derived by reflection.
+      bool InheritsFrom(StackFrame stackFrame, string baseNameTest)
+      {
+        var testType = stackFrame.GetMethod().DeclaringType;
+        while(testType != null)
+        {
+          if (testType.Name == baseNameTest)
+          {
+            return true;
+          }
+          testType = testType.BaseType;
+        }
+        return false;
+      }
+
+      // Get all the stack frames up to the point where this object was created. Omit the constructors for this object.
+      StackTrace stackTrace = new StackTrace(true);
+      var baseName = typeof(ConcurrentObservableBase<T, TInternalCollection>).Name; ;
+      _stackFrames = stackTrace.GetFrames().SkipWhile(frame => InheritsFrom(frame, baseName)).ToArray();
+#endif
       _lock = isMultithreaded ? new ReaderWriterLockSlim() : null;
       _internalCollection = initialCollection;
       _viewChanged = new ThrottledAction(() => OnPropertyChanged(nameof(CollectionView), nameof(Count)), TimeSpan.FromMilliseconds(20));
@@ -177,7 +207,13 @@ namespace Swordfish.NET.Collections
           var name = value.Target?.GetType().FullName;
           if (name == "System.Windows.Data.CollectionView" || name == "System.Windows.Data.ListCollectionView")
           {
-            throw new ApplicationException($"Collection type={typeof(T).Name}, don't bind directly to {nameof(ConcurrentObservableCollection<T>)}, instead bind to {nameof(ConcurrentObservableCollection<T>)}.CollectionView");
+            string stackFrame = "";
+            if (_stackFrames!=null)
+            {
+              stackFrame = $" List created at line {_stackFrames[0].GetFileLineNumber()} in {_stackFrames[0].GetFileName()}.";
+            }
+            // Note if you find yourself here in a debug session, you can get the stack call of where this list was created in _stackFrames. You're welcome.
+            throw new ApplicationException($"Collection type={typeof(T).Name}, don't bind directly to {nameof(ConcurrentObservableCollection<T>)}, instead bind to {nameof(ConcurrentObservableCollection<T>)}.CollectionView. {stackFrame}");
           }
           // Try binding to CollectionView instead
           // Note that if you do comment out the above you'll get an inconsistent
