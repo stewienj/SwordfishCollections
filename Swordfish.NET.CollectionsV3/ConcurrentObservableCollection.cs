a3d64ddf49c54f78ae6605475e1e4f3f64cf767c
John Stewien <stewienj@users.noreply.github.com>
2020-10-01 14:05:11 +0930

    Made the new editable CollectionView a separate property so as to make it a non breaking change. It's now EditableCollectionView. Refactored ConcurrentObservableCollection to make the code tighter and more modern.

--- Swordfish.NET.CollectionsV3/ConcurrentObservableCollection.cs
+++ Swordfish.NET.CollectionsV3/ConcurrentObservableCollection.cs
@@ -11,7 +11,7 @@ using System.Threading;
 
 namespace Swordfish.NET.Collections
 {
-	public interface IConcurrentObservableList<T> : IConcurrentObservableBase<T>, IList<T>, IList 
+    public interface IConcurrentObservableList<T> : IConcurrentObservableBase<T>, IList<T>, IList
     {
         ImmutableList<T> ImmutableList { get; }
     };
@@ -35,9 +35,10 @@ namespace Swordfish.NET.Collections
     {
         /// <summary>
         /// The CollectionView, used in Binding to a ListView or DataGrid is immutable, thus readonly,
-        /// this bridge passes the writes from the CollectionView back to the underlying collection.
+        /// this bridge passes the writes from the CollectionView back to the underlying collection,
+        /// and handles some syncronization issues.
         /// </summary>
-        private EditableImmutableListBridge<T> _collectionView;
+        private EditableImmutableListBridge<T> _editableCollectionView;
 
         public ConcurrentObservableCollection() : this(true)
         {
@@ -51,173 +52,123 @@ namespace Swordfish.NET.Collections
         /// <param name="isThreadSafe"></param>
         public ConcurrentObservableCollection(bool isMultithreaded) : base(isMultithreaded, ImmutableList<T>.Empty)
         {
-            _collectionView = EditableImmutableListBridge<T>.Empty(this);
+            _editableCollectionView = EditableImmutableListBridge<T>.Empty(this);
         }
 
         protected override void OnCollectionChanged(NotifyCollectionChangedEventArgs changes)
         {
             // Assign new value to CollectionView so it is recognised as different to existing value
-            _collectionView = _collectionView.UpdateSource((ImmutableList<T>)_internalCollection);
+            _editableCollectionView = _editableCollectionView.UpdateSource((ImmutableList<T>)_internalCollection);
             base.OnCollectionChanged(changes);
         }
 
+        public ImmutableList<T> ImmutableList => (ImmutableList<T>)_internalCollection;
 
-		public ImmutableList<T> ImmutableList
-        {
-            get
-            {
-                return (ImmutableList<T>)_internalCollection;
-            }
-        }
-
-        protected virtual int IListAdd(T item)
-        {
-            return DoReadWriteNotify(
+        protected virtual int IListAdd(T item) =>
+            DoReadWriteNotify(
               () => ImmutableList.Count,
               (index) => ImmutableList.Add(item),
               (index) => new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Add, item, index)
             );
-        }
 
-        public T RemoveLast()
-        {
-            return DoReadWriteNotify(
+        public T RemoveLast() =>
+            DoReadWriteNotify(
               () => new { Index = ImmutableList.Count - 1, Item = ImmutableList.LastOrDefault() },
               (indexAndItem) => indexAndItem.Index < 0 ? ImmutableList : ImmutableList.RemoveAt(indexAndItem.Index),
               (indexAndItem) => new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Remove, indexAndItem.Item)
             ).Item;
-        }
 
         /// <summary>
         /// Adds a range of items to the end of the collection. Quicker than adding them individually,
         /// but the view doesn't update until the last item has been added.
         /// </summary>
-        public virtual void AddRange(IList<T> items)
-        {
+        public virtual void AddRange(IList<T> items) =>
             DoReadWriteNotify(
               () => ImmutableList.Count,
               (index) => ImmutableList.AddRange(items),
               (index) => new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Add, (IList)items, index)
             );
-        }
 
         /// <summary>
         /// Inserts a range of items at the position specified. *Much quicker* than adding them
         /// individually, but the view doesn't update until the last item has been inserted.
         /// </summary>
-        public virtual void InsertRange(int index, IList<T> items)
-        {
+        public virtual void InsertRange(int index, IList<T> items) =>
             DoWriteNotify(
               () => ImmutableList.InsertRange(index, items),
               () => new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Add, (IList)items, index)
             );
-        }
 
         /// <summary>
         /// Rmoves a range of items by index and count
         /// </summary>
-        public void RemoveRange(int index, int count)
-        {
+        public void RemoveRange(int index, int count) =>
             DoReadWriteNotify(
               () => ImmutableList.GetRange(index, count),
               (items) => ImmutableList.RemoveRange(index, count),
               (items) => new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Remove, (IList)items, index)
             );
-        }
 
-        public void RemoveRange(IList<T> items)
-        {
+        public void RemoveRange(IList<T> items) =>
             DoWriteNotify(
               () => ImmutableList.RemoveRange(items),
               () => new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Remove, (IList)items)
             );
-        }
 
-        public void Reset(IList<T> items)
-        {
+        public void Reset(IList<T> items) =>
             DoReadWriteNotify(
               () => ImmutableList.ToArray(),
               (oldItems) => ImmutableList<T>.Empty.AddRange(items),
               (oldItems) => new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Remove, (IList)oldItems, 0),
               (oldItems) => new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Add, (IList)items, 0)
             );
-        }
 
-        public T[] ToArray()
-        {
-            return ImmutableList.ToArray();
-        }
+        public T[] ToArray() => ImmutableList.ToArray();
 
-        public List<T> ToList()
-        {
-            return ImmutableList.ToList();
-        }
+        public List<T> ToList() => ImmutableList.ToList();
 
-        public override string ToString()
-        {
-            return $"{{Items : {Count}}}";
-        }
-
+        public override string ToString() => $"{{Items : {Count}}}";
 
         // ************************************************************************
         // IEnumerable<T> Implementation
         // ************************************************************************
         #region IEnumerable<T> Implementation
 
-        public IEnumerator<T> GetEnumerator()
-        {
-            return ImmutableList.GetEnumerator();
-        }
+        public IEnumerator<T> GetEnumerator() => ImmutableList.GetEnumerator();
 
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            return ImmutableList.GetEnumerator();
-        }
+        IEnumerator IEnumerable.GetEnumerator() => ImmutableList.GetEnumerator();
 
         #endregion IEnumerable<T> Implementation
-
 
         // ************************************************************************
         // IList<T> Implementation
         // ************************************************************************
         #region IList<T> Implementation
 
-        public int IndexOf(T item)
-        {
-            return ImmutableList.IndexOf(item);
-        }
+        public int IndexOf(T item) => ImmutableList.IndexOf(item);
 
-        public virtual void Insert(int index, T item)
-        {
+        public virtual void Insert(int index, T item) =>
             DoWriteNotify(
               () => ImmutableList.Insert(index, item),
               () => new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Add, item, index)
             );
-        }
 
-        public void RemoveAt(int index)
-        {
+        public void RemoveAt(int index) =>
             DoReadWriteNotify(
               () => ImmutableList[index],
               (item) => ImmutableList.RemoveAt(index),
               (item) => new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Remove, item, index)
             );
-        }
 
         public virtual T this[int index]
         {
-            get
-            {
-                return ImmutableList[index];
-            }
-            set
-            {
+            get => ImmutableList[index];
+            set =>
                 DoReadWriteNotify(
                   () => ImmutableList[index],
                   (item) => ImmutableList.SetItem(index, value),
                   (item) => new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Replace, value, item, index)
                 );
-            }
         }
 
         #endregion IList<T> Implementation
@@ -227,25 +178,16 @@ namespace Swordfish.NET.Collections
         // ************************************************************************
         #region ICollection<T> Implementation
 
-        public void Add(T item)
-        {
-            IListAdd(item);
-        }
+        public void Add(T item) => IListAdd(item);
 
-        public void Clear()
-        {
+        public void Clear() =>
             DoReadWriteNotify(
               () => ImmutableList.ToArray(),
               (items) => ImmutableList.Clear(),
               (items) => new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Remove, items, 0)
             );
-        }
 
-        public bool Contains(T item)
-        {
-            return ImmutableList.Contains(item);
-        }
-
+        public bool Contains(T item) => ImmutableList.Contains(item);
 
         // There's some interplay between Count and CopyTo where an array/list is created
         // and copied to. Unfortunately the collection can change between these two calls.
@@ -275,32 +217,27 @@ namespace Swordfish.NET.Collections
             }
         }
 
-        public override int Count
-        {
-            get
-            {
-                return ImmutableList.Count;
-            }
-        }
+        public override int Count => ImmutableList.Count;
 
-        public bool IsReadOnly
-        {
-            get
-            {
-                return false;
-            }
-        }
+        public bool IsReadOnly => false;
 
-        public override IList<T> CollectionView => _collectionView;
+        public override IList<T> CollectionView => _internalCollection;
 
-        public bool Remove(T item)
-        {
-            return DoReadWriteNotify(
+        /// <summary>
+        /// Bind to this property in the ItemSource of a WPF DataGrid to allow editing
+        /// of the collection from the view. I could have made the CollectionView property
+        /// editable, but I didn't want to change the item that was returned as it might
+        /// have broken someones code that depended on the return type being an immutable
+        /// collection.
+        /// </summary>
+        public IList<T> EditableCollectionView => _editableCollectionView;
+
+        public bool Remove(T item) =>
+            DoReadWriteNotify(
               () => ImmutableList.IndexOf(item),
               (index) => index < 0 ? ImmutableList : ImmutableList.RemoveAt(index),
               (index) => new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Remove, item, index)
             ) >= 0;
-        }
 
         #endregion ICollection<T> Implementation
 
@@ -309,26 +246,11 @@ namespace Swordfish.NET.Collections
         // ************************************************************************
         #region ICollection Implementation
 
-        void ICollection.CopyTo(Array array, int index)
-        {
-            ((ICollection)ImmutableList).CopyTo(array, index);
-        }
+        void ICollection.CopyTo(Array array, int index) => ((ICollection)ImmutableList).CopyTo(array, index);
 
-        bool ICollection.IsSynchronized
-        {
-            get
-            {
-                return ((ICollection)ImmutableList).IsSynchronized;
-            }
-        }
+        bool ICollection.IsSynchronized => ((ICollection)ImmutableList).IsSynchronized;
 
-        object ICollection.SyncRoot
-        {
-            get
-            {
-                return ((ICollection)ImmutableList).SyncRoot;
-            }
-        }
+        object ICollection.SyncRoot => ((ICollection)ImmutableList).SyncRoot;
 
         #endregion ICollection Implementation
 
@@ -337,64 +259,29 @@ namespace Swordfish.NET.Collections
         // ************************************************************************
         #region IList Implementation
 
-        int IList.Add(object value)
-        {
-            return (value is T item) ?
-             IListAdd(item) :
-             -1;
-        }
+        int IList.Add(object value) =>
+            (value is T item) ?
+                IListAdd(item) :
+                -1;
 
-        bool IList.Contains(object value)
-        {
-            return ((IList)ImmutableList).Contains(value);
-        }
+        bool IList.Contains(object value) => ((IList)ImmutableList).Contains(value);
 
-        int IList.IndexOf(object value)
-        {
-            return ((IList)ImmutableList).IndexOf(value);
-        }
+        int IList.IndexOf(object value) => ((IList)ImmutableList).IndexOf(value);
 
-        void IList.Insert(int index, object value)
-        {
-            Insert(index, (T)value);
-        }
+        void IList.Insert(int index, object value) => Insert(index, (T)value);
 
-        bool IList.IsFixedSize
-        {
-            get
-            {
-                return ((IList)ImmutableList).IsFixedSize;
-            }
-        }
+        bool IList.IsFixedSize => ((IList)ImmutableList).IsFixedSize;
 
-        bool IList.IsReadOnly
-        {
-            get
-            {
-                return ((IList)ImmutableList).IsReadOnly;
-            }
-        }
+        bool IList.IsReadOnly => ((IList)ImmutableList).IsReadOnly;
 
-        void IList.Remove(object value)
-        {
-            Remove((T)value);
-        }
+        void IList.Remove(object value) => Remove((T)value);
 
-        void IList.RemoveAt(int index)
-        {
-            RemoveAt(index);
-        }
+        void IList.RemoveAt(int index) => RemoveAt(index);
 
         object IList.this[int index]
         {
-            get
-            {
-                return this[index];
-            }
-            set
-            {
-                this[index] = (T)value;
-            }
+            get => this[index];
+            set => this[index] = (T)value;
         }
 
         #endregion IList Implementation
@@ -403,6 +290,7 @@ namespace Swordfish.NET.Collections
         // ISerializable Implementation
         // ************************************************************************
         #region ISerializable Implementation
+
         protected override void GetObjectData(SerializationInfo info, StreamingContext context)
         {
             base.GetObjectData(info, context);
@@ -413,6 +301,7 @@ namespace Swordfish.NET.Collections
         {
             _internalCollection = System.Collections.Immutable.ImmutableList.CreateRange((T[])information.GetValue("children", typeof(T[])));
         }
-        #endregion
+
+        #endregion ISerializable Implementation
     }
 }
