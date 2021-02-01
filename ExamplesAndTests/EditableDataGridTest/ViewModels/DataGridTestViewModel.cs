ccc36c4037a11878e881cc648841a30f0fad721e
John Stewien <stewienj@users.noreply.github.com>
2021-02-01 18:06:53 +1030

    Updated the Editable DataGrid Test to have items continously change and / or have items continuously added when activated by check boxes.

--- ExamplesAndTests/EditableDataGridTest/ViewModels/DataGridTestViewModel.cs
+++ ExamplesAndTests/EditableDataGridTest/ViewModels/DataGridTestViewModel.cs
@@ -15,14 +15,20 @@ namespace EditableDataGridTest.ViewModels
     public class DataGridTestViewModel : INotifyPropertyChanged
     {
         private Random _random = new Random();
+        private System.Threading.Timer _addItemTimer;
+        private System.Threading.Timer _updateItemTimer;
+
         public DataGridTestViewModel()
         {
+            _addItemTimer = new System.Threading.Timer(AddRandomItemFromTimer, null, System.Threading.Timeout.Infinite, System.Threading.Timeout.Infinite);
+            _updateItemTimer = new System.Threading.Timer(UpdateRandomItemFromTimer, null, System.Threading.Timeout.Infinite, System.Threading.Timeout.Infinite);
+
             TestCollection.PropertyChanged += (s, e) =>
             {
                 switch (e.PropertyName)
                 {
                     case nameof(ConcurrentObservableCollection<TestItem>.CollectionView):
-                        PropertyChanged?.Invoke(this,new PropertyChangedEventArgs(nameof(TestCollectionView)));
+                        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(TestCollectionView)));
                         break;
                     case nameof(ConcurrentObservableCollection<TestItem>.EditableCollectionView):
                         PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(EditableTestCollectionView)));
@@ -32,6 +38,24 @@ namespace EditableDataGridTest.ViewModels
 
             var randomItems = Enumerable.Range(0, 10).Select(i => GetRandomItem());
             TestCollection.AddRange(randomItems);
+        }
+
+        /// <summary>
+        /// This method is called by the timer delegate. Adds a random item.
+        /// </summary>
+        private void AddRandomItemFromTimer(Object stateInfo)
+        {
+            TestCollection.Add(GetRandomItem());
+        }
+
+        /// <summary>
+        /// This method is called by the timer delegate. Updates a random item.
+        /// </summary>
+        private void UpdateRandomItemFromTimer(Object stateInfo)
+        {
+            var selectedItem = TestCollection[_random.Next(TestCollection.Count)];
+            selectedItem.Value1 = _random.Next().ToString();
+            selectedItem.Value2 = _random.Next().ToString();
         }
 
         private TestItem GetRandomItem() => new TestItem
@@ -44,9 +68,86 @@ namespace EditableDataGridTest.ViewModels
         private RelayCommandFactory _addRandomItemCommand = new RelayCommandFactory();
         public ICommand AddRandomItemCommand => _addRandomItemCommand.GetCommand(() => TestCollection.Add(GetRandomItem()));
 
+        private bool _continuouslyAddItems = false;
+        public bool ContinuouslyAddItems
+        {
+            get => _continuouslyAddItems;
+            set
+            {
+                _continuouslyAddItems = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(ContinuouslyAddItems)));
+                if (_continuouslyAddItems)
+                {
+                    _addItemTimer.Change(0, 1000);
+                }
+                else
+                {
+                    _addItemTimer.Change(System.Threading.Timeout.Infinite, System.Threading.Timeout.Infinite);
+                }
+            }
+        }
+
+        private bool _continuouslyUpdateItems = false;
+        public bool ContinuouslyUpdateItems
+        {
+            get => _continuouslyUpdateItems;
+            set
+            {
+                _continuouslyUpdateItems = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(ContinuouslyAddItems)));
+                if (_continuouslyUpdateItems)
+                {
+                    _updateItemTimer.Change(0, 100);
+                }
+                else
+                {
+                    _updateItemTimer.Change(System.Threading.Timeout.Infinite, System.Threading.Timeout.Infinite);
+                }
+            }
+        }
+
+
         /// <summary>
-        /// The source collection
+        /// The main source source collection. You can bing to TestCollection.CollectionView or bind to TestCollectionView,
+        /// they are both the same thing, but TestCollectionView shows how to shorten the path to the ItemsSource source.
+        /// For an editable DataGrid you can bind to TestCollection.EditableCollectionView or bind to the EditableTestCollectionView
+        /// property further down. Again they are both the same thing, just the latter is a shorter path.
         /// </summary>
+        public ConcurrentObservableCollection<TestItem> TestCollection { get; }  = new ConcurrentObservableCollection<TestItem>();
+        /// <summary>
+        /// This is the read-only collection view
+        /// </summary>
+        public IList<TestItem> TestCollectionView => TestCollection.CollectionView;
+        /// <summary>
+        /// This is an editable collection that can be edited in a DataGrid. 
+        /// </summary>
+        public IList<TestItem> EditableTestCollectionView => TestCollection.EditableCollectionView;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+
+    /// <summary>
+    /// This is an example view model, it's not used in this application, but rather it
+    /// is featured in the README.md
+    /// </summary>
+    public class ExampleViewModel : INotifyPropertyChanged
+    {
+        public ExampleViewModel()
+        {
+            TestCollection.PropertyChanged += (s, e) =>
+            {
+                switch (e.PropertyName)
+                {
+                    case nameof(ConcurrentObservableCollection<TestItem>.CollectionView):
+                        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(TestCollectionView)));
+                        break;
+                    case nameof(ConcurrentObservableCollection<TestItem>.EditableCollectionView):
+                        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(EditableTestCollectionView)));
+                        break;
+                }
+            };
+        }
+
         public ConcurrentObservableCollection<TestItem> TestCollection { get; }
             = new ConcurrentObservableCollection<TestItem>();
 
@@ -55,33 +156,6 @@ namespace EditableDataGridTest.ViewModels
 
         public event PropertyChangedEventHandler PropertyChanged;
     }
-
-public class ExampleViewModel : INotifyPropertyChanged
-{
-    public ExampleViewModel()
-    {
-        TestCollection.PropertyChanged += (s, e) =>
-        {
-            switch (e.PropertyName)
-            {
-                case nameof(ConcurrentObservableCollection<TestItem>.CollectionView):
-                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(TestCollectionView)));
-                    break;
-                case nameof(ConcurrentObservableCollection<TestItem>.EditableCollectionView):
-                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(EditableTestCollectionView)));
-                    break;
-            }
-        };
-    }
-
-    public ConcurrentObservableCollection<TestItem> TestCollection { get; }
-        = new ConcurrentObservableCollection<TestItem>();
-
-    public IList<TestItem> TestCollectionView => TestCollection.CollectionView;
-    public IList<TestItem> EditableTestCollectionView => TestCollection.EditableCollectionView;
-
-    public event PropertyChangedEventHandler PropertyChanged;
-}
 
     /// <summary>
     /// Test item used to populate the test collection
