4eaced5f43dedffc780dfad5a800a1cc6564ad03
John Stewien <stewienj@users.noreply.github.com>
2021-04-08 10:45:31 +0930

    Added just a bit more to the DataGrid sample, I should stop thinking about it now

--- ExamplesAndTests/DataGridGroupSortFilterUltimateExample/Controls/DataGridEditableTestControlMVVM.xaml.cs
+++ ExamplesAndTests/DataGridGroupSortFilterUltimateExample/Controls/DataGridEditableTestControlMVVM.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using DataGridGroupSortFilterUltimateExample.ViewModels;
 using System.Windows.Controls;
+using System.Windows.Data;
 
 namespace DataGridGroupSortFilterUltimateExample.Controls
 {
@@ -12,6 +13,32 @@ namespace DataGridGroupSortFilterUltimateExample.Controls
         {
             DataContext = new DataGridConcurrentTestViewModel();
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// Handles when the Multi-Selected Items DataGrid adds columns to allow instant changing of
+        /// value.s Don't do it with the main grid because there's an exception thrown due to a sort
+        /// occuring during an edit.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void DataGrid_AutoGeneratingColumn(object sender, DataGridAutoGeneratingColumnEventArgs e)
+        {
+            if (e.Column is DataGridComboBoxColumn comboBoxColumn)
+            {
+                if (comboBoxColumn.SelectedItemBinding is Binding binding)
+                {
+                    binding.UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged;
+                }
+            }
+            else if (e.Column is DataGridBoundColumn boundColumn)
+            {
+                if (boundColumn.Binding is Binding binding)
+                {
+                    binding.UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged;
+                }
+            }
+
         }
     }
 }
