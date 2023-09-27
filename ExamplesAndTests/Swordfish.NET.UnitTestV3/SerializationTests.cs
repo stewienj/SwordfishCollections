6170861ba4fd8b79c9ee7e29bf8a68d5a8ebf612
John Stewien <stewienj@users.noreply.github.com>
2023-09-27 15:54:49 +0930

    #29 Ported project files to VS2022, updated all the nuget packages

--- ExamplesAndTests/Swordfish.NET.UnitTestV3/SerializationTests.cs
+++ ExamplesAndTests/Swordfish.NET.UnitTestV3/SerializationTests.cs
@@ -1,39 +1,57 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Swordfish.NET.Collections;
+using System;
 using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
+#if NETCOREAPP
+using System.Text.Json;
+#endif
 
 namespace Swordfish.NET.UnitTestV3
 {
     [TestClass]
     public class SerializationTests
     {
-        [TestMethod]
-        public void ConcurrentObservableCollectionSerializationTest()
+#if NETCOREAPP
+        private T RoundTripSerialization<T>(T collection) where T : class
+        {
+            var bytes = JsonSerializer.SerializeToUtf8Bytes(collection,
+                        new JsonSerializerOptions { WriteIndented = false });
+            return JsonSerializer.Deserialize<T>(new ReadOnlySpan<byte>(bytes));
+        }
+#else
+        private T RoundTripSerialization<T>(T collection) where T : class
         {
             var serializer = new BinaryFormatter();
             var stream = new MemoryStream();
+            serializer.Serialize(stream, collection);
+            stream.Position = 0;
+            return serializer.Deserialize(stream) as T;
+        }
+#endif
+
+        [TestMethod]
+        public void ConcurrentObservableCollectionSerializationTest()
+        {
             var collection = new ConcurrentObservableCollection<string>();
             for (int i = 0; i < 10; i++)
                 collection.Add("TestItem" + (i + 1).ToString());
-            serializer.Serialize(stream, collection);
-            stream.Position = 0;
-            collection = serializer.Deserialize(stream) as ConcurrentObservableCollection<string>;
+
+            collection = RoundTripSerialization(collection);
+
             for (int i = 0; i < 10; i++)
                 Assert.AreEqual("TestItem" + (i + 1).ToString(), collection[i]);
         }
 
-        [TestMethod]
+            [TestMethod]
         public void ConcurrentObservableDictionarySerializationTest()
         {
-            var serializer = new BinaryFormatter();
-            var stream = new MemoryStream();
             var collection = new ConcurrentObservableDictionary<string, int>();
             for (int i = 0; i < 10; i++)
                 collection.Add("TestItem" + (i + 1).ToString(), i);
-            serializer.Serialize(stream, collection);
-            stream.Position = 0;
-            collection = serializer.Deserialize(stream) as ConcurrentObservableDictionary<string, int>;
+
+            collection = RoundTripSerialization(collection);
+
             for (int i = 0; i < 10; i++)
                 Assert.AreEqual(i, collection["TestItem" + (i + 1).ToString()]);
         }
@@ -41,14 +59,12 @@ namespace Swordfish.NET.UnitTestV3
         [TestMethod]
         public void ConcurrentObservableSortedDictionarySerializationTest()
         {
-            var serializer = new BinaryFormatter();
-            var stream = new MemoryStream();
             var collection = new ConcurrentObservableSortedDictionary<string, int>();
             for (int i = 0; i < 10; i++)
                 collection.Add("TestItem" + (i + 1).ToString(), i);
-            serializer.Serialize(stream, collection);
-            stream.Position = 0;
-            collection = serializer.Deserialize(stream) as ConcurrentObservableSortedDictionary<string, int>;
+
+            collection = RoundTripSerialization(collection);
+
             for (int i = 0; i < 10; i++)
                 Assert.AreEqual(i, collection["TestItem" + (i + 1).ToString()]);
         }
@@ -56,14 +72,12 @@ namespace Swordfish.NET.UnitTestV3
         [TestMethod]
         public void ConcurrentObservableHashSetSerializationTest()
         {
-            var serializer = new BinaryFormatter();
-            var stream = new MemoryStream();
             var collection = new ConcurrentObservableHashSet<string>();
             for (int i = 0; i < 10; i++)
                 collection.Add("TestItem" + (i + 1).ToString());
-            serializer.Serialize(stream, collection);
-            stream.Position = 0;
-            collection = serializer.Deserialize(stream) as ConcurrentObservableHashSet<string>;
+
+            collection = RoundTripSerialization(collection);
+
             Assert.AreEqual(10, collection.Count);
             for (int i = 0; i < 10; i++)
                 Assert.IsTrue(collection.Contains("TestItem" + (i + 1).ToString()));
